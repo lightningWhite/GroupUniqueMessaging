ba7b7c8a17b143cb787940cacafee5e87760b5e0
Daniel Hornberger <hor12020@byui.edu>
2017-11-01 17:17:37 -0600

    Added two more unit tests for MsgParser to check for no bracket at end

--- app/src/test/java/com/gum/dlt/groupuniquemessaging/MsgParserUnitTest.java
+++ app/src/test/java/com/gum/dlt/groupuniquemessaging/MsgParserUnitTest.java
@@ -7,7 +7,8 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 /**
- * Created by tayle on 11/1/2017.
+ * Created by Tayler on 11/1/2017.
+ * These unit tests are for the MsgParser class.
  */
 
 public class MsgParserUnitTest {
@@ -19,19 +20,32 @@ public class MsgParserUnitTest {
 
     @Test
     public void test_getVar_names() throws Exception{
-        String msg1 = "Hello <name> with you come to <place>?";
+
+        // Normal case
+        String msg1 = "Hello <name> will you come to <place>?";
         MsgParser parser = new MsgParser(msg1);
         List<String> variables = parser.getVariableNames();
         assertEquals(variables.get(0), "name");
         assertEquals(variables.get(1), "place");
 
-        String msg2 = "Hello <name with you come to <place>?";
+        // Missing end bracket in middle of message
+        String msg2 = "Hello <name will you come to <place>?";
         MsgParser parser1 = new MsgParser(msg2);
         variables = parser1.getVariableNames();
-        assertEquals(variables.get(0), "name with you come to <place");
+        assertEquals(variables.get(0), "name will you come to <place");
 
+        // Missing beginning bracket in middle of message
+        String msg3 = "Hello name> will you come to <place>?";
+        MsgParser parser2 = new MsgParser(msg3);
+        variables = parser2.getVariableNames();
+        assertEquals(variables.get(0), "place");
 
-
+        // Missing missing closing bracket at end of message
+        String msg4 = "Hello <name> will you come to <place";
+        MsgParser parser3 = new MsgParser(msg4);
+        variables = parser3.getVariableNames();
+        assertEquals(variables.get(0), "name");
+        assertEquals(variables.get(1), "place");
     }
 }
 
