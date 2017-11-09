acbf063e700b993a4814963ddfc00508fb37ad4c
Daniel Hornberger <hor12020@byui.edu>
2017-11-09 15:14:32 -0700

    Modified the ContactListViewTask so it uses the custom ContactsAdapter

--- app/src/main/java/com/gum/dlt/groupuniquemessaging/ContactListViewTask.java
+++ app/src/main/java/com/gum/dlt/groupuniquemessaging/ContactListViewTask.java
@@ -2,23 +2,21 @@ package com.gum.dlt.groupuniquemessaging;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.List;
 
 /**
  * This class provides a threaded process to add a list of contacts to the contact ListView
  */
-public class ContactListViewTask extends AsyncTask<Void, String, Void> {
+public class ContactListViewTask extends AsyncTask<Void, Contact, Void> {
 
-    ArrayAdapter<String> _arrayAdapter;
+    ContactsAdapter _contactsAdapter;
     List<Contact> _contacts;
     Context _currentContext;
 
-    ContactListViewTask(ArrayAdapter<String> arrayAdapter, List<Contact> contacts, Context currentContext) {
-        _arrayAdapter = arrayAdapter;
+    ContactListViewTask(ContactsAdapter contactsAdapter, List<Contact> contacts, Context currentContext) {
+        _contactsAdapter = contactsAdapter;
         _contacts = contacts;
         _currentContext = currentContext;
     }
@@ -35,7 +33,7 @@ public class ContactListViewTask extends AsyncTask<Void, String, Void> {
 
         // Traverse throught the list of contacts and publish them
         for (Contact contact: _contacts) {
-            publishProgress(contact.get_contact());
+            publishProgress(contact);
         }
 
         return null;
@@ -43,10 +41,10 @@ public class ContactListViewTask extends AsyncTask<Void, String, Void> {
 
     // This runs on the main thread so it can modify the GUI
     @Override
-    protected void onProgressUpdate(String... values) {
+    protected void onProgressUpdate(Contact... values) {
         super.onProgressUpdate(values);
         // Add each contact to the ListView
-        _arrayAdapter.add(values[0]);
+        _contactsAdapter.add(values[0]);
     }
 
     // This class runs on the main thread
