c69a5e6b882e01bce5b9524b681a359526c2b06b
Daniel Hornberger <hor12020@byui.edu>
2017-11-10 17:48:47 -0700

    Made it so the keyboard is enabled but doesn't push up the buttons

--- app/src/main/java/com/gum/dlt/groupuniquemessaging/MainActivity.java
+++ app/src/main/java/com/gum/dlt/groupuniquemessaging/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -57,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        // This makes it so the keyboard doesn't push up the buttons
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
 
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},1);
 
