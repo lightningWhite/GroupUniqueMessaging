5fb571ef91315cb9dca044ffb7329e6fbfab2b8f
TaylerDB <bak12001@byui.edu>
2017-12-04 17:46:42 -0700

    Added code so that it checks for null contacts and spaces before contact name

--- app/src/main/java/com/gum/dlt/groupuniquemessaging/ContactParser.java
+++ app/src/main/java/com/gum/dlt/groupuniquemessaging/ContactParser.java
@@ -23,16 +23,28 @@ public class ContactParser {
     }
 
     public String getContactFirstName(String fullContactName) {
-        _firstName = parse(fullContactName);
+        if (fullContactName != null) {
+            _firstName = parse(fullContactName);
+        }
+
         return _firstName;
     }
 
     private String parse(String contactLabel) {
+        if(contactLabel == null){
+            return contactLabel;
+        }
+
         String firstName = "";
 
-        // Place each character of the variable into a string until end of block or end of
-        // string
+        // Place each character of the variable into a string until end of block or end of string
         int i = 0;
+
+        // Check for a space before the contact
+        while (i < contactLabel.length() && contactLabel.charAt(i) == ' ')
+            i++;
+
+        // Parse the contact
         while (i < contactLabel.length() && contactLabel.charAt(i) != ' ') {
             firstName += contactLabel.charAt(i);
             // Go to the next letter in the variable
