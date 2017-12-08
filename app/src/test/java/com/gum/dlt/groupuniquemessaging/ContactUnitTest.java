0438e57afe891baeae56701ed490286dd4a7f5c6
TaylerDB <bak12001@byui.edu>
2017-12-08 14:34:45 -0700

    Added more unit test to the ContactUnitTest class

--- app/src/test/java/com/gum/dlt/groupuniquemessaging/ContactUnitTest.java
+++ app/src/test/java/com/gum/dlt/groupuniquemessaging/ContactUnitTest.java
@@ -21,29 +21,55 @@ public class ContactUnitTest {
      */
     @Test
     public void test_getFirstName_normal() throws Exception{
+        // First and Last Name
         String name = "John Doe";
         Contact contact = new Contact();
         contact.set_contactName(name);
         String expectedFirstName = "John";
         String actualFirstName = contact.get_firstName();
         assertEquals(expectedFirstName, actualFirstName);
+
+        // One name
+        String name2 = "John";
+        Contact contact2 = new Contact();
+        contact2.set_contactName(name);
+        String expectedFirstName2 = "John";
+        String actualFirstName2 = contact2.get_firstName();
+        assertEquals(expectedFirstName2, actualFirstName2);
     }
 
     /**
-     * Checks for a odd or wierd input from user
+     * Checks for a odd spaces in the contact
      * @throws Exception
      */
     @Test
-    public void test_getFirstName_crazy() throws Exception{
+    public void test_getFirstName_OddSpc() throws Exception{
+        // Space before contact
         String name = " John Doe";
         Contact contact = new Contact();
         contact.set_contactName(name);
         String expectedFirstName = "John";
         String actualFirstName = contact.get_firstName();
-
         System.out.format("This is the the actual first name: %s", actualFirstName);
-
         assertEquals(expectedFirstName, actualFirstName);
+
+        //Space after contact
+        String name2 = "John Doe ";
+        Contact contact2 = new Contact();
+        contact2.set_contactName(name2);
+        String expectedFirstName2 = "John";
+        String actualFirstName2 = contact2.get_firstName();
+        System.out.format("This is the the actual first name: %s", actualFirstName2);
+        assertEquals(expectedFirstName2, actualFirstName2);
+
+        //Space after contact
+        String name3 = "John  Doe";
+        Contact contact3 = new Contact();
+        contact3.set_contactName(name3);
+        String expectedFirstName3 = "John";
+        String actualFirstName3 = contact.get_firstName();
+        System.out.format("This is the the actual first name: %s", actualFirstName3);
+        assertEquals(expectedFirstName3, actualFirstName3);
     }
 
     /**
@@ -79,10 +105,5 @@ public class ContactUnitTest {
         for (int i = 0; i < variables.size(); i++){
             assertEquals(variables.get(i), expected);
         }
-
     }
-
-
-
-
 }
