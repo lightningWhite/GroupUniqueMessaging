46c98b174092db011a9942ca9805e557921f3133
squigglemufkinz <sum11011@byui.edu>
2017-12-08 14:45:36 -0700

    Message Unit test added for get message with set variables.

--- app/src/test/java/com/gum/dlt/groupuniquemessaging/MessageUnitTest.java
+++ app/src/test/java/com/gum/dlt/groupuniquemessaging/MessageUnitTest.java
@@ -27,7 +27,16 @@ public class MessageUnitTest {
     }
 
     @Test
-    public void Test_get_msg_with_set_variables(List<String> variables) {
-        
+    public void Test_get_msg_with_set_variables() {
+        String title = "The Title";
+        String msgTemplate = "Hello <contact> could you meet with bishop on <day> at <time>?";
+
+        Message msg = new Message(title, msgTemplate);
+        List<String > myList = msg.get_variable_names_from_template();
+
+        msg.set_msg_template(msg._msgTemplate);
+        String msgActual = msg.get_msg_with_set_variables(myList);
+
+        assertEquals(msgActual, "Hello contact could you meet with bishop on day at time?");
     }
 }
