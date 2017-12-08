d25791653e08c3a55d64d83c01a707c383d123bf
squigglemufkinz <sum11011@byui.edu>
2017-12-08 14:35:28 -0700

    Message Unit test added

---
+++ app/src/test/java/com/gum/dlt/groupuniquemessaging/MessageUnitTest.java
@@ -0,0 +1,33 @@
+package com.gum.dlt.groupuniquemessaging;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by logan on 12/8/2017.
+ */
+
+public class MessageUnitTest {
+
+    @Test
+    public void Test_get_variable_names_from_template() {
+        String title = "The Title";
+        String msgTemplate = "Hello <name> could you meet with bishop on <day> at <time>?";
+
+        Message msg = new Message(title, msgTemplate);
+
+        List<String> myList = msg.get_variable_names_from_template();
+
+        assertEquals(myList.get(0), "name");
+        assertEquals(myList.get(1), "day");
+        assertEquals(myList.get(2), "time");
+    }
+
+    @Test
+    public void Test_get_msg_with_set_variables(List<String> variables) {
+        
+    }
+}
